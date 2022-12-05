ff0553836f909620041360452b85d085068d9c41
erickson-m <mutisyaerickson@gmail.com>
2022-12-05 17:58:48 +0000

    replacing in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+
+
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+        return my_list
+    else:
+        return my_list
+
+
+if __name__ == "__main__":
+    replace_list()
