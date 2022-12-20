71fa7406b898a3e5dc72020681abaaff2f7b2613
erickson-m <mutisyaerickson@gmail.com>
2022-12-20 22:50:04 +0000

    added option for float

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,8 +8,30 @@ def list_division(my_list_1, my_list_2, list_length):
             type_a = isinstance(a, int)
             type_b = isinstance(b, int)
             if False in [type_a, type_b]:
-                print("Wrong type")
-                result.append(0)
+                #check for float
+                float_a = isinstance(a, float)
+                float_b = isinstance(b, float)
+                if type_a is False and type_b is False:
+                    if float_a is True and float_b is True:
+                        ans = a / b
+                        result.append(ans)
+                    else:
+                        print("Wrong type")
+                        result.append(0)
+                elif type_a is False and type_b is True:
+                    if float_a is True:
+                        ans = a / b
+                        result.append(ans)
+                    else:
+                        print("Wrong type")
+                        result.append(0)
+                elif type_b is False and type_a is True:
+                    if float_a is True:
+                        ans = a / b
+                        result.append(ans)
+                    else:
+                        print("Wrong type")
+                        result.append(0)
             elif b == 0:
                 print("division by 0")
                 result.append(0)
