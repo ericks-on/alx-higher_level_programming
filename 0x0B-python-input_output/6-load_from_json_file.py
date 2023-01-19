e46738a11d50c9a3740069a9fc04272dd29b32b3
erickson-m <mutisyaerickson@gmail.com>
2023-01-19 19:28:22 +0000

    string to object

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module contains one function which creates an object from a json file
+
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """this function creates object from a JSON file
+
+    Attributes:
+        obj_str: json string rep of object
+        the_obj: the object finally deserialized
+
+    Args:
+        filename(string): the name of the file
+
+    """
+    with open(filename, encoding='utf-8') as f:
+        obj_str = f.read()
+        the_obj = json.load(obj_str)
+        return (the_obj)
