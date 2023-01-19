6a460aab81117abee7c13460a72d54e562cc0b3d
erickson-m <mutisyaerickson@gmail.com>
2023-01-19 15:55:23 +0000

    add instead of overwriting

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""contains function to append contents in a file
+
+"""
+def append_write(filename="", text=""):
+    """this function appends text at the end of a file
+    Attributes:
+        n: the number of characters that are written to file
+
+    Args:
+        filename(string): the name of the file
+        text(string): text to add to file
+
+    Returns:
+        int: the number of characters written to file
+
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        n = f.write(text)
+    return (n)
