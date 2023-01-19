e8a9f0dfb6f374767c6d9fd7b37948d3333e6b1d
erickson-m <mutisyaerickson@gmail.com>
2023-01-19 18:21:28 +0000

    from json to object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """this module contains function that serializes using json
 """
 
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+import json
+"""module contains oly one function using json
+"""
 
+def from_json_string(my_str):
+    """this function returns an object rep by a JSON string
+
+    Args:
+        my_str: JSON representation of an object
+
+    Return:
+        object that is represented by my_str
+
+    """
+    r = json.loads(my_str)
+    return (r)
