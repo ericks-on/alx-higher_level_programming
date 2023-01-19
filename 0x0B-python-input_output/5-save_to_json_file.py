58d46b08a27ea53bc643a2679332c06d401ad8b2
erickson-m <mutisyaerickson@gmail.com>
2023-01-19 19:16:17 +0000

    save object to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""this module contains one function which saves object to file
+"""
 
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """this function saves an object in json rep to file
+
+    Args:
+        my_obj: object to save
+        filename(string): name of the file
+
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
