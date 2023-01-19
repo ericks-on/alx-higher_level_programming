a60450f090fd99be22286dc5fde1a2a82a64bd58
erickson-m <mutisyaerickson@gmail.com>
2023-01-19 16:07:07 +0000

    json dumps

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""this module contains function that serializes using json
+"""
+def to_json_string(my_obj):
+    """this function returns JSON rep of object passed
+
+    Args:
+        my_obj: object to serialize
+
+    Return:
+        string: the JSON representation of the object 
+
+    """
+    r = json.dumps(my_obj)
+    return(r)
