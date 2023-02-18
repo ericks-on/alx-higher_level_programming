e9b5c4a4fb5189ae5b6c6c267c5f3615cb6bc46e
erickson <mutisyaerickson@gmail.com>
2023-02-18 05:59:50 -0800

    specify the columns to get description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- full description of table
-SELECT *
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY
 FROM information_schema.COLUMNS
 WHERE TABLE_NAME = 'first_table'
 AND TABLE_SCHEMA = 'hbtn_0c_0'
