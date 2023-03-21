e9eb59040dc8878ea3e19e916c0ab26d8f3b1f2e
erickson mbuvi <mutisyaerickson@gmail.com>
2023-03-21 17:50:09 +0300

    list all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""uses MySQLdb to list cities in a database
+"""
+
+import sys
+import MySQLdb as sql
+
+
+def get_cities(*args, **kargs):
+    """lists all cities from a database
+
+    Args:
+        username: username of the mysql server
+        password: password of the user
+        database: the database to obtain the cities
+
+    """
+    try:
+        db = sql.connect(user=sys.argv[1], password=sys.argv[2],
+                         host='localhost', port=3306, db=sys.argv[3])
+    except sql.Error as e:
+        raise (e)
+    else:
+        query = "SELECT * FROM cities ORDER BY id"
+        c = db.cursor()
+        c.execute(query)
+        result = c.fetchall()
+        for row in result:
+            print(row)
+        c.close()
+        db.close()
+
+
+if __name__ == '__main__':
+    get_cities()
