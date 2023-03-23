c696bec6461e4911c8c4350e0a659280b9f6f5e7
erickson mbuvi <mutisyaerickson@gmail.com>
2023-03-23 22:19:05 +0300

    fixed some few errors

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,8 +12,17 @@ Base = declarative_base()
 
 
 class State(Base):
+    """this class inherits from base and links to mysql table states
+
+    Attributes:
+        id: column that represents a unique auto generated integer and is
+            the primary key
+        name: another attribute that represents the names
+    """
     __tablename__ = 'states'
     id = Column(Integer, Sequence('states_id_seq'), primary_key=True,
                 nullable=False, unique=True)
     name = Column(String(128), nullable=False)
 
+    def __repr__(self):
+        return (f"<State(id={self.id}, name={self.name})>")
